6710bccacf56c6105ce3f2b5e04aac61c2f67a2a
Tony Tsui <tsui.tony@gmail.com>
2017-04-30 10:57:15 +1000

    Show background image based on weather condition

--- src/App.js
+++ src/App.js
@@ -5,6 +5,15 @@ import "./App.css";
 import AppBar from "./AppBar";
 import WeatherPane from "./WeatherPane";
 import { DAYS, WEATHER_COND } from "./constants";
+import backgroundCloudy from "./images/background-cloudy.png";
+import backgroundPartlyCloudy from "./images/background-partly-cloudy.png";
+import backgroundSunny from "./images/background-sunny.png";
+
+const backgroundByWeatherCondition = {
+  [WEATHER_COND.CLOUDY]: backgroundCloudy,
+  [WEATHER_COND.PARTLY_CLOUDY]: backgroundPartlyCloudy,
+  [WEATHER_COND.SUNNY]: backgroundSunny
+};
 
 const store = {
   currentDay: DAYS.THU,
@@ -49,17 +58,30 @@ const store = {
 
 class App extends Component {
   render() {
+    const currentDayWeather = store.data.find(data => data.dayOfWeek === store.currentDay);
     return (
       <div className="App">
-        <AppBar />
+        <div style={{
+               position: "absolute" ,
+               width: "100%",
+               zIndex: 100
+             }} >
+          <AppBar />
+        </div>
 
         <ReactSwipe>
           {
-            store.data.map((data, idx) => (
-              <div key={idx} >
+            store.data.map((data, idx) => {
+              const i = 1;
+              return (
+              <div key={idx}
+                     style={{
+                       backgroundImage: `url(${backgroundByWeatherCondition[data.weatherCondition]})`,
+                     }}>
                 <WeatherPane key={idx} dayOfWeek={ data.dayOfWeek } conditions={ data } />
               </div>
-            ))
+            )}
+              )
           }
         </ReactSwipe>
 
